6cd3338efa13f7bb1479960b1f3b487c09c399c9
msweelam <sweelam.fcih2014@gmail.com>
2024-04-11 01:04:45 +0400

    add new lecture

---
+++ code-examples/nodejs-examples/api-semantics/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/api/orders', (req, res) => {
+  res.json({"msg": "Hello from Express!"})
+})
+
+app.post('/api/orders', (req, res) => {
+    let contentType = req.headers['content-type']
+
+    if (contentType !== 'application/json') 
+        res.sendStatus(415)     // unsupported media type
+    
+
+    res.json({"msg": "empty response ^_^"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
