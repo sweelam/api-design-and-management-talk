4be5ff164f4e41ae1a8fd34d54abda97185bc67f
Sweelam <sweelam.fcih2014@gmail.com>
2024-08-04 15:50:03 +0400

    Introduce Auth Manager

--- code-examples/java-examples/flight-system-microservices/flight-user-service/src/main/java/com/users/config/SecurityConfig.java
+++ code-examples/java-examples/flight-system-microservices/flight-user-service/src/main/java/com/users/config/SecurityConfig.java
@@ -1,20 +1,35 @@
 package com.users.config;
 
+import com.users.filter.HeaderBasedFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    public static final String ADMIN = "SweProfile";
+    private final AuthProvider authProvider;
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(authProvider);
+        return authenticationManagerBuilder.build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,9 +42,8 @@ public class SecurityConfig {
                             .requestMatchers("/favicon.ico").permitAll()
                             .requestMatchers("/api/public/users/**").permitAll()
                 )
-                .formLogin(withDefaults())
-//                .addFilterBefore(new HeaderBasedFilter() , UsernamePasswordAuthenticationFilter.class)
-//                .authenticationProvider(new SuperAdminAuthenticationProvider())
+                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(new HeaderBasedFilter() , UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
