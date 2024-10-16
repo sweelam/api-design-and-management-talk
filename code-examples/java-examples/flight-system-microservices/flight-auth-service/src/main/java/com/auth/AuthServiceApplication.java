7d2c0a74c456a80a239b5c77892a1de91d02c871
Sweelam <sweelam.fcih2014@gmail.com>
2024-10-16 06:09:34 +0400

    dummy commit

--- code-examples/java-examples/flight-system-microservices/flight-auth-service/src/main/java/com/auth/AuthServiceApplication.java
+++ code-examples/java-examples/flight-system-microservices/flight-auth-service/src/main/java/com/auth/AuthServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AuthServiceApplication.class, args);
+  }
 
 }
--- code-examples/java-examples/flight-system-microservices/flight-user-service/src/main/java/com/auth/UserServiceApplication.java
+++ code-examples/java-examples/flight-system-microservices/flight-user-service/src/main/java/com/auth/UserServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(UserServiceApplication.class, args);
+  }
 
 }
